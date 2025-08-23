37a58cb60632c6bf75e0de7af3c297aa113c68f8
dmitriy <dmitriy_yz@dmitriys-MacBook-Pro.local>
2025-08-23 09:30:15 +0300

    Add func enter value from user

--- 1-converter/main.go
+++ 1-converter/main.go
@@ -7,10 +7,22 @@ const (
 	DOLLARS_TO_RUB  = 92.50
 )
 
+
+func enterUserInput() {
+	fmt.Println("Enter amount in USD:")
+	var amount float64
+	fmt.Scan(&amount)
+}
+
+func converCurrency(amount float64, origin string, target string) {}
+
 func main() {
 	euroToRub := DOLLARS_TO_RUB / DOLLARS_TO_EURO
 
 	fmt.Printf("USD to EUR %.2f\n", DOLLARS_TO_EURO)
 	fmt.Printf("USD to RUB %.2f\n", DOLLARS_TO_RUB)
 	fmt.Printf("EUR to RUB %.2f\n", euroToRub)
+
+	enterUserInput()
 }
+
